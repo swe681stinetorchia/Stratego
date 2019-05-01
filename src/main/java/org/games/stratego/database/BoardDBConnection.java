5545b3023870f6be65e0cbeac651171a46b36452
Torchia <Jonathan.Torchia@calibresys.com>
2019-05-01 16:12:39 -0400

    Added BoardDBConnection.java

---
+++ src/main/java/org/games/stratego/database/BoardDBConnection.java
@@ -0,0 +1,24 @@
+package org.games.stratego.database;
+
+public class BoardDBConnection extends StrategoDBConnection {
+
+    public String getPiece(String col_name, String game_id)
+    {
+        String returnVal = "";
+
+        try {
+            preparedStatement = connect
+                    .prepareStatement("select ? from board where game_id=?");
+            preparedStatement.setString(1, col_name);
+            preparedStatement.setString(2, game_id);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                returnVal = resultSet.getString(col_name);
+            }
+        } catch (Exception e) {
+            log.fatal(e.getMessage());
+        }
+        return returnVal;
+    }
+}
--- src/main/java/org/games/stratego/database/StrategoDBConnection.java
+++ src/main/java/org/games/stratego/database/StrategoDBConnection.java
@@ -36,28 +36,4 @@ public class StrategoDBConnection {
             log.fatal(e.getMessage());
         }
     }
-
-    public String getPiece(String col_name, String game_id)
-    {
-        String returnVal = "";
-
-        try {
-            preparedStatement = connect
-                    .prepareStatement("select ? from board where game_id=?");
-            preparedStatement.setString(1, col_name);
-            preparedStatement.setString(2, game_id);
-            resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                returnVal = resultSet.getString(2);
-                System.out.println(returnVal);
-            }
-        } catch (Exception e) {
-            log.fatal(e.getMessage());
-        }
-        return returnVal;
-    }
-
-
-
 }
