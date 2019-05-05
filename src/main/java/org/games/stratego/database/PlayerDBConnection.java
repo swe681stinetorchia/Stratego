23610de0201db124e9710401102a38279909ca82
Torchia <Jonathan.Torchia@calibresys.com>
2019-05-05 18:14:29 -0400

    Updated datalayer to use int for ids

--- src/main/java/org/games/stratego/database/PlayerDBConnection.java
+++ src/main/java/org/games/stratego/database/PlayerDBConnection.java
@@ -13,38 +13,14 @@ import org.apache.logging.log4j.Logger;
 
 public class PlayerDBConnection extends StrategoDBConnection {
 
-    protected Connection connect = null;
-    protected Statement statement = null;
-    protected PreparedStatement preparedStatement = null;
-    protected ResultSet resultSet = null;
-    protected final Logger log = LogManager.getLogger(getClass());
-    protected String url;
-    protected String username;
-    protected String password;
-
-    public PlayerDBConnection() {
-        try {
-            StrategoGetPropertyValues config = new StrategoGetPropertyValues();
-            url = config.getPropValues("dbURL");
-            username = config.getPropValues("username");
-            password = config.getPropValues("password");
-            // This will load the MySQL driver, each DB has its own driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (Exception e) {
-            log.fatal(e.getMessage());
-        }
-    }
-    public void addPlayer(String userID)
+    public void addPlayer(int userID)
     {
 
         try {
-            // Setup the connection with the DB
-            connect = DriverManager
-                    .getConnection(url, username, password);
 
             preparedStatement = connect
-                    .prepareStatement("insert into stratego.player (UUID(), ?, SYSDATE())");
-            preparedStatement.setString(1, userID);
+                    .prepareStatement("insert into stratego.player (user_id, lastLogin) VALUES (?, SYSDATE())");
+            preparedStatement.setInt(1, userID);
             resultSet = preparedStatement.executeQuery();
             connect.close();
         }
--- src/main/java/org/games/stratego/model/admin/game.java
+++ src/main/java/org/games/stratego/model/admin/game.java
@@ -1,4 +1,15 @@
 package org.games.stratego.model.admin;
 
+import java.sql.Date;
+
 public class game {
+   int id;
+    int player_one;
+    int player_two;
+   Date startTime;
+   int winner;
+   int loser;
+   int nextTurn
+
+
 }
