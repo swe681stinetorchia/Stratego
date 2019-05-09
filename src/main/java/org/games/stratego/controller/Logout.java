1679ee8d0209c56b09ecc6ba1099d861562362dc
Torchia <Jonathan.Torchia@calibresys.com>
2019-05-09 14:29:29 -0400

    Added SSL changes to web.xml

--- src/main/java/org/games/stratego/controller/Logout.java
+++ src/main/java/org/games/stratego/controller/Logout.java
@@ -18,7 +18,9 @@ import java.io.IOException;
 public class Logout extends HttpServlet {
     private final Logger log = LogManager.getLogger(getClass());
 
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
